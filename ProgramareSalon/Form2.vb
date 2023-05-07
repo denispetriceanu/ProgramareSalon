a60ba87ee39c19865789b204d803c133fe7e3a4a
Petriceanu Denis <49166081+denispetriceanu@users.noreply.github.com>
2023-05-07 20:20:59 +0300

    Last changed - function for hour - save corect

--- ProgramareSalon/Form2.vb
+++ ProgramareSalon/Form2.vb
@@ -3,7 +3,7 @@ Imports System.Globalization
 Imports System.Net.Mail
 
 Public Class Form2
-
+    Dim orar(20) As String 'definim un array cu 24 de elemente, pentru orele de la 8: 00 la 18:00
     Dim mailaddress As MailAddress
     Dim name As String
     Dim lastName As String
@@ -15,6 +15,7 @@ Public Class Form2
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Me.TextBox1.AppendText("")
+        ArrHours()
         Me.DateTimePicker1.Value = DateTime.Today.AddDays(-1)
         DateTimePicker1.Format = DateTimePickerFormat.Custom
         DateTimePicker1.CustomFormat = "yyyy-MM-dd"
@@ -27,33 +28,86 @@ Public Class Form2
     End Sub
 
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
-
         Dim date1 As DateTime = DateTime.ParseExact(DateTime.Today.ToString("yyyy-MM-dd"), "yyyy-MM-dd", CultureInfo.InvariantCulture)
         Dim date2 As DateTime = DateTime.ParseExact(DateTimePicker1.Value().ToString("yyyy-MM-dd"), "yyyy-MM-dd", CultureInfo.InvariantCulture)
         If date1 <= date2 Then
-            ' complet withe possible hour in list
-            ' search for hour disponible
             Label9.Text = ""
+            ' complet withe possible hour in list
+            ' get our with ocupated
+            Dim con As OleDbConnection = New OleDbConnection
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=A:\CristinaProject\ProgramareSalon\SalonDB.accdb"
+            con.Open()
+
+            Dim query As String = "SELECT Ora FROM Programari where Data = #" + date2 + "#"
+            Dim command As New OleDbCommand(query, con)
+
+            Dim adapter As New OleDbDataAdapter(command)
+            ' save data in array
+            Dim dataset As New DataSet()
+            adapter.Fill(dataset, "Programari")
+            Dim table As DataTable = dataset.Tables("Programari")
+            Dim array(table.Rows.Count - 1) As String
+
+            For i As Integer = 0 To table.Rows.Count - 1
+                Dim row As DataRow = table.Rows(i)
+                For j As Integer = 0 To table.Columns.Count - 1
+                    Dim timp As DateTime = DateTime.ParseExact(row(j), "h:mm:ss tt", CultureInfo.InvariantCulture)
+                    Dim timpFormatat As String = timp.ToString("hh:mm tt")
+                    array(i) = timpFormatat
+                Next
+            Next
+
             ComboBox1.Items.Clear()
-            ComboBox1.Items.Insert(0, "07:11")
-            ComboBox1.Items.Insert(0, "10:11")
-            ComboBox1.Items.Insert(1, "10:12")
-            ComboBox1.Items.Insert(2, "10:13")
-            ComboBox1.Items.Insert(3, "10:15")
+            array.Reverse(orar)
+            For Each ora As String In orar
+                If Not array.Contains(ora) Then
+                    'If Not ora Is Nothing Then
+                    ComboBox1.Items.Insert(0, ora)
+                    'End If
+                End If
+            Next
         Else
             Label9.Text = "Setează o dată validă"
         End If
     End Sub
 
+    Private Function ArrHours()
+        Dim startTime As New TimeSpan(8, 0, 0) ' Ora de inceput
+        Dim endTime As New TimeSpan(18, 0, 0) ' Ora de sfarsit
+        Dim interval As New TimeSpan(0, 30, 0) ' Intervalul de timp
+
+        'Dim orar((CInt((endTime - startTime).TotalMinutes) \ CInt(interval.TotalMinutes)) + 1) As String
+
+
+        Dim oraCurenta As TimeSpan = startTime
+        For i As Integer = 0 To orar.Length - 1
+            Dim timp As DateTime = DateTime.ParseExact(oraCurenta.ToString(), "HH:mm:ss", CultureInfo.InvariantCulture)
+            Dim timpConvert As String = DateTime.Parse(timp.ToString).ToString("hh:mm tt")
+            orar(i) = timpConvert
+            oraCurenta = oraCurenta.Add(interval)
+        Next
+    End Function
+
+
+    Private Function CompareDate()
+        Dim date1 As DateTime = DateTime.ParseExact(DateTime.Today.ToString("yyyy-MM-dd"), "yyyy-MM-dd", CultureInfo.InvariantCulture)
+        Dim date2 As DateTime = DateTime.ParseExact(DateTimePicker1.Value().ToString("yyyy-MM-dd"), "yyyy-MM-dd", CultureInfo.InvariantCulture)
+        If date1 = date2 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
         'ToDo
         'Step 1: check complete all information
-        ' CheckData()
+        Dim test As Boolean = CheckData()
 
         'Step 2: add the databse
         'Create connection string
-        If CheckData() Then
+        If test Then
             Dim con As OleDbConnection = New OleDbConnection
             Try
                 'To Connect the Database
@@ -78,9 +132,20 @@ Public Class Form2
         End If
 
         'Step 3: message to check if want to add another or getout
-
         'Step 4: clean forms
-
+        If test Then
+            Dim result As DialogResult = MessageBox.Show("Mai adăugați alte programări?", "Programare", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                Me.TextBox3.Text = ""
+                Me.TextBox4.Text = ""
+                Me.TextBox6.Text = ""
+                Me.TextBox5.Text = ""
+                Me.TextBox8.Text = ""
+            Else
+                HomePage.Show()
+                Me.Close()
+            End If
+        End If
 
 
     End Sub
@@ -89,7 +154,8 @@ Public Class Form2
         Dim now As DateTime = DateTime.Now
         Dim hour As String = now.ToString("HH")
         Dim minute As String = now.ToString("mm")
-        Return hour & ":" & minute
+        Dim period As String = now.ToString("tt")
+        Return hour & ":" & minute & " " & period
     End Function
 
     Private Function CheckData() As Boolean
@@ -100,6 +166,13 @@ Public Class Form2
         data_plan = Me.DateTimePicker1.Value
         hour = Me.ComboBox1.SelectedItem
         phone = Me.TextBox8.Text
+
+        'Me.TextBox3.Text = "Test"
+        'Me.TextBox4.Text = "Test"
+        'Me.TextBox6.Text = "test@test.com"
+        'Me.TextBox5.Text = "test-obs"
+        'Me.TextBox8.Text = "0740663926"
+
 
         If name.Length <= 3 Then
             Label9.Text = "Numele nu a fost setat sau nu este valid"
@@ -129,12 +202,17 @@ Public Class Form2
         '    Label9.Text = "Ora nu a fost selectată"
         '    Return False
         'End If
-        Dim time1 As TimeSpan = TimeSpan.ParseExact(getTimeNow.ToString, "hh\:mm", Nothing)
-        Dim time2 As TimeSpan = TimeSpan.ParseExact(hour, "hh\:mm", Nothing)
-        If time2 < time1 Then
-            Label9.Text = "Nu a fost selectată o ora valida"
-            Return False
+        Dim time1 As DateTime = DateTime.ParseExact(getTimeNow(), "h:mm tt", CultureInfo.InvariantCulture)
+        If Not hour = Nothing Then
+            Dim time2 As DateTime = DateTime.ParseExact(hour, "hh\:mm tt", CultureInfo.InvariantCulture)
+            If time2 < time1 And CompareDate() Then
+                Label9.Text = "nu a fost selectată o ora valida"
+                Return False
+            End If
+        Else
+            Label9.Text = "Nu a fost selectată ora"
         End If
+
         If phone.Length <> 10 Then
             Label9.Text = "Adaugă numărul de telefon sau  introdu unul valid"
             Return False
